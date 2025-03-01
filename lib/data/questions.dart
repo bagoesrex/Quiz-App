f045cb2a18a4e83dd6d599ee864e8ddb45d4bf3f
bagoesrex <bagoesrex@gmail.com>
2025-03-02 03:18:54 +0700

    feat: implement questions list

---
+++ lib/data/questions.dart
@@ -0,0 +1,44 @@
+import 'package:quiz_app/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    'Apa elemen sihir yang dikuasai oleh Megumin?',
+    ['Ledakan', 'Api', 'Petir', 'Es'],
+  ),
+  QuizQuestion(
+    'Apa nama ras Megumin?',
+    ['Crimson Demon', 'Elf', 'Human', 'Demon Lord'],
+  ),
+  QuizQuestion(
+    'Apa warna mata Megumin?',
+    ['Merah', 'Biru', 'Hijau', 'Kuning'],
+  ),
+  QuizQuestion(
+    'Apa nama jurus andalan Megumin?',
+    ['Explosion', 'Fireball', 'Thunder Strike', 'Blizzard'],
+  ),
+  QuizQuestion(
+    'Siapa yang menjadi rival sekaligus sahabat Megumin?',
+    ['Yunyun', 'Aqua', 'Darkness', 'Wiz'],
+  ),
+  QuizQuestion(
+    'Apa yang terjadi setelah Megumin menggunakan sihir ledakan?',
+    ['Dia tidak bisa bergerak', 'Dia semakin kuat', 'Dia bisa menggunakan lagi', 'Dia berubah menjadi lebih besar'],
+  ),
+  QuizQuestion(
+    'Apa ciri khas pakaian Megumin?',
+    ['Jubah merah dan topi penyihir', 'Baju zirah', 'Kimono', 'Jubah putih'],
+  ),
+  QuizQuestion(
+    'Apa yang sering dikatakan Megumin setelah melemparkan sihir ledakannya?',
+    ['EXPLOSION!', 'BOOM!', 'KA-BOOM!', 'DETONATE!'],
+  ),
+  QuizQuestion(
+    'Apa hewan peliharaan Megumin?',
+    ['Chomusuke', 'Pochi', 'Tama', 'Shiro'],
+  ),
+  QuizQuestion(
+    'Kenapa Megumin hanya menggunakan satu jenis sihir?',
+    ['Karena dia mencintai ledakan', 'Karena dia tidak bisa belajar yang lain', 'Karena dia dikutuk', 'Karena itu warisan keluarganya'],
+  ),
+];
